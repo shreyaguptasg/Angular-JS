d8a446f7de825c96667960452f9534dff1e1d7ed
shrey <shreya272002gupta@gmail.com>
2025-03-07 12:30:19 +0530

    [Shreya] Update. User inputted name in uc4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
+// import { AppRoutingModule } from "./app-routing.module";
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, FormsModule],
+    imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
