fc991dbe51f2369a0ee05a623eddd679ea153ed4
shrey <shreya272002gupta@gmail.com>
2025-03-07 12:28:56 +0530

    [Shreya] Update. User inputted name in uc4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -12,6 +13,7 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
+  userName: string = "";
 
   ngOnInit() {
     this.title = "Hello from BridgeLabz";
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
