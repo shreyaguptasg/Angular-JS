2efa53a67fa723b21eb2020bf2c18106305226af
shrey <shreya272002gupta@gmail.com>
2025-03-07 12:18:56 +0530

    [Shreya] Update. Opening new Tab on Clicking the image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit() {
     this.title = "Hello from BridgeLabz";
+  }
+
+  onClick($event: MouseEvent) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
